5d8a195a9b989dafa5d377d9626abcd8c12c9727
Степан Бокарев <bokarevstepan1@yandex.ru>
2024-02-16 21:50:37 +0300

    update

--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -2,11 +2,16 @@ package org.openjfx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
+    //Вот внедренный компонент с fx:id = "mainButton"
+    @FXML
+    private Button primaryButton;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+      //  primaryButton.setText("hello");
     }
 }
